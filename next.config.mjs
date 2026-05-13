b9ca34e2620a186dd50c21135a2c101a8b7ce50c
conradmorten <morten.conrad@stud.fhgr.ch>
2026-05-13 11:51:37 +0200

    Fix basePath for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/Co-Founder',
+  assetPrefix: '/Co-Founder',
   eslint: { ignoreDuringBuilds: true },
 }
 export default nextConfig
